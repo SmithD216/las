13ac95a6b4cc440bed9705a2c8ccc553408a2332
Dylan Smith <smithd216@protonmail.com>
2018-01-02 21:43:02 +0000

    The real final update to the countdown script.

--- users/management/commands/countdown.py
+++ users/management/commands/countdown.py
@@ -2,4 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from users.models import Member
 
 class Command(BaseCommand):
-    Member.objects.update(submissions_today=0)
+    help = "The countdown script that resets the daily submission counter of the users."
+    def handle(self, *args, **options):
+        Member.objects.update(submissions_today=0)
+        self.stdout.write("Daily submissions have been successfuly reset to zero.")
