c4fb5e3a62d8966b5c49c8e0a26686a0704ca458
Dylan Smith <smithd216@protonmail.com>
2018-01-02 21:31:06 +0000

    Im not even sure about this one.

--- users/management/commands/countdown.py
+++ users/management/commands/countdown.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand, CommandError
 from users.models import Member
 
 class Command(BaseCommand):
+
+    def handle(self, *args, **options)
     Member.objects.update(submissions_today=0)
 
 self.stdout.write("Daily submissions successfully reset.")
