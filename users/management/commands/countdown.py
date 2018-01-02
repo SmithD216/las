d819814c587206f29cef7d2a488153b4f65c945d
Dylan Smith <smithd216@protonmail.com>
2018-01-02 21:27:43 +0000

    I was wrong about the cron script.

--- users/management/commands/countdown.py
+++ users/management/commands/countdown.py
@@ -1,2 +1,7 @@
+from django.core.management.base import BaseCommand, CommandError
 from users.models import Member
-Member.objects.update(submissions_today=0)
+
+class Command(BaseCommand):
+    Member.objects.update(submissions_today=0)
+
+self.stdout.write("Daily submissions successfully reset.")
